360c449a2b24cfe55ceea2319cf4af368a220562
Frank Moran <info@francisfrankmoran.com>
2020-01-16 19:46:22 -0500

    ajax - current city, temp, humidity, wind speed

---
+++ Assets/script.js
@@ -0,0 +1,149 @@
+// Easy access to elements
+var searchHistoryList = $('#search-history-list');
+var searchCityInput = $("#search-city");
+var searchCityButton = $("#search-city-button");
+var clearHistoryButton = $("#clear-history");
+
+var currentCity = $("#current-city");
+var currentTemp = $("#current-temp");
+var currentHumidity = $("#current-humidity");
+var currentWindSpeed = $("#current-wind-speed");
+
+// Get access to the OpenWeather API
+var APIkey = "a17e1499228be1f9c294ac18b234c7d7";
+
+// Easy access to data
+var cityList = [];
+
+// Check if search history exists when page loads
+initalizeHistory();
+showClear();
+
+
+
+// Hitting enter while input is focused will trigger
+// value added to search history
+$(document).on("submit", function(){
+    event.preventDefault();
+
+    // Grab value entered into search bar 
+    var searchValue = searchCityInput.val().trim();
+
+    currentConditionsRequest(searchValue)
+    searchHistory(searchValue);
+    searchCityInput.val(""); 
+})
+
+// Clicking the search button will trigger
+// value added to search history
+searchCityButton.on("click", function(){
+    event.preventDefault();
+
+    // Grab value entered into search bar 
+    var searchValue = searchCityInput.val().trim();
+
+    currentConditionsRequest(searchValue)
+    searchHistory(searchValue);    
+    searchCityInput.val(""); 
+});
+
+// Clear the sidebar of past cities searched
+clearHistoryButton.on("click", function(){
+    // Empty out the  city list array
+    cityList = [];
+    // Update city list history in local storage
+    listArray();
+    
+    $(this).addClass("hide");
+});
+
+
+// Request Open Weather API based on user input
+function currentConditionsRequest(searchValue) {
+    
+    // Formulate URL for AJAX api call
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial&appid=" + APIkey;
+
+    // Make AJAX call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        currentCity.text(response.name);
+        currentTemp.text(response.main.temp);
+        currentHumidity.text(response.main.humidity);
+        currentWindSpeed .text(response.wind.speed);
+    });
+
+}
+
+// Display and save the search history of cities
+function searchHistory(searchValue) {
+    // Grab value entered into search bar 
+    // var searchValue = searchCityInput.val().trim();
+    
+    // If there are characters entered into the search bar
+    if (searchValue) {
+        // Place value in the array of cities
+        // if it is a new entry
+        if (cityList.indexOf(searchValue) === -1) {
+            cityList.push(searchValue);
+
+            // List all of the cities in user history
+            listArray();
+            clearHistoryButton.removeClass("hide");
+        } else {
+            // Remove the existing value from
+            // the array
+            var removeIndex = cityList.indexOf(searchValue);
+            cityList.splice(removeIndex, 1);
+
+            // Push the value again to the array
+            cityList.push(searchValue);
+
+            // list all of the cities in user history
+            // so the old entry appears at the top
+            // of the search history
+            listArray();
+            clearHistoryButton.removeClass("hide");
+        }
+    }
+    // console.log(cityList);
+}
+
+// List the array into the search history sidebar
+function listArray() {
+    // Empty out the elements in the sidebar
+    searchHistoryList.empty();
+    // Repopulate the sidebar with each city
+    // in the array
+    cityList.forEach(function(city){
+        var searchHistoryItem = $('<li class="list-group-item">');
+        searchHistoryItem.text(city);
+        searchHistoryList.prepend(searchHistoryItem);
+    });
+    // Update city list history in local storage
+    localStorage.setItem("cities", JSON.stringify(cityList));
+}
+
+// Grab city list string from local storage
+// and update the city list array
+// for the search history sidebar
+function initalizeHistory() {
+    if (localStorage.getItem("cities")) {
+        cityList = JSON.parse(localStorage.getItem("cities"));
+        // console.log(cityList);
+        listArray();
+    }
+}
+
+// Check to see if there are elements in
+// search history sidebar in order to show clear history btn
+function showClear() {
+    if (searchHistoryList.text() !== "") {
+        clearHistoryButton.removeClass("hide");
+    }
+}
+// console.log(searchHistoryList.text());
+// console.log(cityList);
